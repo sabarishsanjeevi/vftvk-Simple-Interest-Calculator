d753c120189111575ac75c81b35f53a4fe85e351
Sabarish Sanjeevi <57557106+sabarishsanjeevi@users.noreply.github.com>
2021-07-14 05:52:18 +0000

    project-level-1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var p = document.getElementById("principal").value;
+   var r = document.getElementById("rate").value;
+   var y = document.getElementById("years").value;
+    var si = (p*y*r)/100;
 }
         
